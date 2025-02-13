f69e84d151f04ce208bbbfbaa55bb5f0aa5f0829
anusha chatterjee <anushachatterjee@Mac.lan>
2025-02-12 16:37:16 -0800

    added packet struct

--- project/transport.h
+++ project/transport.h
@@ -7,3 +7,16 @@
 void listen_loop(int sockfd, struct sockaddr_in* addr, int type,
                  ssize_t (*input_p)(uint8_t*, size_t),
                  void (*output_p)(uint8_t*, size_t));
+
+
+
+#define MSS 1012
+typedef struct {
+    uint16_t seq;     // Sequence Number
+    uint16_t ack;     // Acknowledgment Number
+    uint16_t length;  // Payload Length
+    uint16_t window;  // Flow Control Window
+    uint16_t flags;   // Flags (SYN, ACK, P)
+    uint16_t unused;  // Unused Space
+    uint8_t payload[MSS];  // Data Payload
+} packet;
